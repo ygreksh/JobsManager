46863587c2239c1b7d563455cb3ffa490d2c6ac4
ygreksh <ygreksh@gmail.com>
2021-09-05 21:48:20 +0300

    1

---
+++ JobsManager/JobExecutor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace JobsManager
+{
+    public class JobExecutor : IJobExecutor
+    {
+        public int Amount { get; }
+        public Semaphore semaphore;
+        public static Dictionary<int, Thread> ThreadsPool = new Dictionary<int, Thread>();
+
+        /*
+        public void JobExecutor()
+        {
+            ThreadsPool = new Dictionary<int, Thread>();
+        }
+        */
+        
+        public void Start(int maxConcurrent)
+        {
+            semaphore = new Semaphore(maxConcurrent, maxConcurrent);
+        }
+
+        public void Stop()
+        {
+            semaphore.Release();
+        }
+
+        public void Add(Action action)
+        {
+            Thread myThread = new Thread(action.Invoke);
+            ThreadsPool.Add(myThread.GetHashCode(),myThread);
+            semaphore.WaitOne();
+        }
+
+        public void Clear()
+        {
+            semaphore.Close();
+        }
+    }
+}
---
+++ JobsManager/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace JobsManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JobExecutor jobExecutor = new JobExecutor();
+            //jobExecutor.Start(3);
+            jobExecutor.Add(() =>
+            {
+                Console.WriteLine("Работа 1");
+                Thread.Sleep(1000);
+            });
+            jobExecutor.Add(() =>
+            {
+                Console.WriteLine("Работа 2");
+                Thread.Sleep(1000);
+            });
+            jobExecutor.Add(() =>
+            {
+                Console.WriteLine("Работа 3");
+                Thread.Sleep(1000);
+            });
+            jobExecutor.Start(3);
+            Thread.Sleep(6000);
+            jobExecutor.Stop();
+            jobExecutor.Clear();
+            Console.ReadKey();
+
+        }
+    }
+}
